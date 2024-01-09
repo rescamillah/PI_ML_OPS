0f4d06ce11a482897a74f22cc7f12f540068e3ad
Jovanni Escamilla <40836283+rescamillah@users.noreply.github.com>
2024-01-09 00:31:31 -0600

    desktop
    Con fast api local corriendo

---
+++ functions.py
@@ -0,0 +1,155 @@
+# ### Funciones para Endpoints / ML
+
+# En esta sección, nos centraremos en consumir los archivos que se generaron en el notebook **Consultas**. que serán utilizadas por la API. El objetivo principal es dar respuestas a los Endpoints y a los Modelos Machine Learning, garantizando rapidez y eficacia en la ejecución de la API.
+
+# Se importan las librerías necesarias
+import pandas as pd
+import os
+
+
+# ### Endpoint 1
+"""
+**def PlayTimeGenre( genero : str ):** Debe devolver año con mas horas jugadas para dicho género.
+
+Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}
+
+Input: **PlayTimeGenre.csv**
+"""
+
+def PlayTimeGenre(genero: str):
+    result_df = pd.read_csv('PlayTimeGenre.csv')
+
+    # Filtrar el DataFrame para el género específico
+    filtered_df = result_df[result_df['genres'] == genero]
+    
+    # Agrupar por año de lanzamiento y sumar las horas jugadas
+    grouped_df = filtered_df.groupby('year')['playtime_forever'].sum()
+    
+    # Encontrar el año con más horas jugadas
+    max_hours_year = grouped_df.idxmax()
+
+    # Construye el response_data
+    response_data = {"Año de lanzamiento con más horas jugadas para {}: {}".format(genero, max_hours_year)}
+
+    # Muestra el resultado
+    return response_data
+
+
+# ### Endpoint 2
+"""
+**def UserForGenre( genero : str ):** Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
+
+Ejemplo de retorno: {"Usuario con más horas jugadas para Género X" : us213ndjss09sdf, "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
+
+Input: **UserForGenre.csv**
+"""
+
+def UserForGenre(genero:str):
+    consulta2 = pd.read_csv('UserForGenre.csv')
+    
+    # Filtrar el DataFrame por el género dado
+    genre_data = consulta2[consulta2['genres'] == genero]
+
+    # Encontrar al usuario con más horas jugadas para ese género
+    top_user = genre_data.loc[genre_data['playtime_forever'].idxmax()]['user_id']
+
+    # Crear una lista de acumulación de horas jugadas por año
+    hours_by_year = genre_data.groupby('year')['playtime_forever'].sum().reset_index()
+  
+    hours_by_year = hours_by_year.rename(columns={'year': 'Año', 'playtime_forever': 'Horas'})
+    
+    hours_list = hours_by_year.to_dict(orient='records')
+
+    # Crear el diccionario de retorno
+    result = {
+        "Usuario con más horas jugadas para Género {}".format(genero): top_user,
+        "Horas jugadas": hours_list
+    }
+
+    return result
+
+# ### Endpoint 3
+"""
+**def UsersRecommend( año : int )**: Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado. (reviews.recommend = True y comentarios positivos/neutrales)
+
+Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+
+Input: **UsersRecommend.csv**
+"""
+
+def UsersRecommend(year: int):
+    df = pd.read_csv('UsersRecommend.csv')
+    
+    # Filtrar el DataFrame por el año especificado
+    result_df = df[df['year'] == year]
+
+    response_data = [{"Puesto 1": result_df.iloc[0]['name']},
+                     {"Puesto 2": result_df.iloc[1]['name']},
+                     {"Puesto 3": result_df.iloc[2]['name']}]
+
+    return response_data
+
+
+# ### Endpoint 4
+"""
+**def UsersWorstDeveloper( año : int )**: Devuelve el top 3 de desarrolladoras con juegos MENOS recomendados por usuarios para el año dado. (reviews.recommend = False y comentarios negativos)
+
+Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+
+Input: **UsersWorstDeveloper.csv**
+"""
+
+def UsersWorstDeveloper(year: int):
+    df = pd.read_csv('UsersWorstDeveloper.csv')
+
+    # Filtrar el DataFrame por el año especificado
+    result_df = df[df['year'] == year]
+    
+    response_data = [{"Puesto 1": result_df.iloc[0]['developer']},
+                    {"Puesto 2": result_df.iloc[1]['developer']},
+                    {"Puesto 3": result_df.iloc[2]['developer']}]
+    
+    return response_data
+
+# Endpoint 5
+"""
+**def sentiment_analysis( empresa desarrolladora : str )**: Según la empresa desarrolladora, se devuelve un diccionario con el nombre de la desarrolladora como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor.
+
+Ejemplo de retorno: {'Valve' : [Negative = 182, Neutral = 120, Positive = 278]}
+
+Input: **sentiment_analysis.csv**
+"""
+
+def sentiment_analysis(empresa_desarrolladora: str):
+    df = pd.read_csv('sentiment_analysis.csv')
+
+    # Filtrar por la empresa desarrolladora
+    result_df = df[df['developer'] == empresa_desarrolladora]
+
+    # Convertir a formato de diccionario
+    response_data = result_df.set_index('developer').to_dict(orient='index')
+    
+    return response_data
+
+
+# ### ML
+
+# ### Recomendación item-item
+
+def recomendacion_por_juego(item_id):
+    df = pd.read_csv('Recomendacion_por_Juego.csv')
+    
+    # Filtrar el DataFrame por el año especificado
+    result_df = df[df['item_id'] == item_id]
+    
+    response_data = result_df['Recomendaciones']
+ 
+    return response_data.head()
+
+# ### ML
+
+# ### Recomendación de juegos por usuario
+
+def recomendacion_por_usuario(id_usuario):
+    
+    return "Función no terminada"
---
+++ main.py
@@ -0,0 +1,213 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+import traceback  
+from typing import List, Dict
+import pandas as pd
+from functions import UsersRecommend, sentiment_analysis, UsersWorstDeveloper, PlayTimeGenre, UserForGenre, recomendacion_por_usuario, recomendacion_por_juego
+
+# Crea una instancia de la aplicación FastAPI
+app = FastAPI()
+# http://127.0.0.1:8000/ Esta es la ruta local de acceso a la fastapi
+@app.get("/")
+def index():
+    return "Hola, bienvenid@ a mi proyecto"
+
+#Endpoint 1
+@app.get("/PlayTimeGenre/{genero}", tags=['PlayTimeGenre'])
+async def endpoint1(genero: str):
+    """
+    Descripción: Esta función retorna el año con más horas jugadas
+    
+    Parámetros:
+        - genero (str): Se recibe el genero para el cuál se consultará el año con mayor horas jugadas
+    
+    Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género (Action)" : 2012}
+    """
+    try:
+        # Validación adicional para asegurarse de que el género no sea nulo o esté vacío
+        if not genero or not genero.strip():
+            raise HTTPException(status_code=422, detail="El parámetro 'genero' no puede ser nulo o estar vacío.")
+
+        result = PlayTimeGenre(genero)
+    
+        # Validación para verificar si el género existe en los datos
+        if not result:
+            raise HTTPException(status_code=404, detail=f"No se encontró información para el género '{genero}'.")
+
+        return result
+    
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=500, detail=f"Error al cargar el archivo PlayTimeGenre.csv: {str(e)}")
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {str(e)}")
+
+
+# ### Endpoint 2
+
+# In[ ]:
+
+
+@app.get("/UserForGenre/{genero}", tags=['UserForGenre'])
+async def endpoint2(genero: str):
+    """
+    Descripción: Retorna el usuario que acumula más horas jugadas para un género dado y una lista de la acumulación de horas jugadas por año.
+
+    Parámetros:
+        - genero (str): Género para el cual se busca el usuario con más horas jugadas. Debe ser un string, ejemplo: Adventure
+
+    Ejemplo de retorno: {"Usuario con más horas jugadas para Género Adventure": Evilutional, Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
+    """
+    try:
+        # Validación adicional para asegurarse de que el género no sea nulo o esté vacío
+        if not genero or not genero.strip():
+            raise HTTPException(status_code=422, detail="El parámetro 'genero' no puede ser nulo o estar vacío.")
+
+        result = UserForGenre(genero)
+        
+        # Validación para verificar si el género existe en los datos
+        if not result:
+            raise HTTPException(status_code=404, detail=f"No se encontró información para el género '{genero}'.")
+            
+        return result
+    
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=500, detail=f"Error al cargar el archivo UserForGenre.csv: {str(e)}")
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {str(e)}")
+
+
+# ### Endpoint 3
+
+# In[ ]:
+
+
+@app.get("/UsersRecommend/{year}", tags=['UsersRecommend'])
+async def endpoint3(year: str):
+    """
+    Descripción: Retorna el top 3 de juegos MÁS recomendados por usuarios para el año dado.
+    
+    Parámetros:
+        - year (str): Año para el cual se busca el top 3 de juegos más recomendados.Debe ser número de 4 digitos, ejemplo: 2015
+
+    Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+    """
+    try:
+        year = int(year)
+    
+        if not (2000 <= year <= 2100):
+            error_message = f"El año debe estar en el rango entre 2000 y 2100 {str(e)}"
+            return JSONResponse(status_code=500, content={"error": error_message})
+        
+        result = UsersRecommend(year)
+    
+        if result:
+            return result
+        else:
+            #raise HTTPException(status_code=404, detail=f"No se encontraron recomendaciones para el año {year}.")
+            error_message = "No se encontraron recomendaciones para el año {year} {str(e)}"
+            return JSONResponse(status_code=500, content={"error": error_message})
+
+    except FileNotFoundError as e:
+        error_message = f"Error al cargar el archivo UsersRecommend.csv: {str(e)}"
+        return JSONResponse(status_code=500, content={"error": error_message})
+
+    except Exception as e:
+        error_message = f"Error interno del servidor: {str(e)}"
+        return JSONResponse(status_code=500, content={"error": error_message})
+
+
+
+# ### Endpoint 4
+
+# In[ ]:
+
+
+@app.get("/UsersWorstDeveloper/{year}", tags=['UsersWorstDeveloper'])
+async def endpoint4(year: str):
+    """
+    Descripción: Retorna el top 3 de desarrolladoras con juegos MENOS recomendados por usuarios para el año dado.
+    
+    Parámetros:
+        - year (str): Año para el cual se busca el top 3 de desarrolladoras menos recomendadas. Debe ser número de 4 digitos, ejemplo: 2015
+
+
+    Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+    """
+    try:
+        year = int(year)
+        result = UsersWorstDeveloper(year)
+        return result
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=500, detail=f"Error al cargar el archivo UsersWorstDeveloper.csv: {str(e)}")
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {str(e)}")
+
+
+# ### Endpoint 5
+
+# In[ ]:
+
+
+@app.get("/sentiment_analysis/{empresa_desarrolladora}", tags=['sentiment_analysis'])
+async def enpoint5(empresa_desarrolladora: str):
+    """
+    Descripción: Según la empresa desarrolladora, se devuelve un diccionario con el nombre de la desarrolladora como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor.
+    
+    Parámetros:
+        - empresa_desarrolladora (str): Nombre de la empresa desarrolladora para la cual se realiza el análisis de sentimiento. Debe ser un string, ejemplo: Valve
+    
+    Ejemplo de retorno: {'Valve' : [Negative = 182, Neutral = 120, Positive = 278]}
+    """
+    try:
+        result = sentiment_analysis(empresa_desarrolladora)
+        return result
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=500, detail=f"Error al cargar el archivo sentiment_analysis.csv: {str(e)}")
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {str(e)}")
+
+
+# ### Sistema de recomendación item-item
+
+# In[ ]:
+
+
+@app.get("/recomendacion_Juegos/{item_id}", tags=['recomendacion por juego'])
+async def item(item_id: int):
+    """
+    Descripción: Ingresando el id de producto, devuelve una lista con 5 juegos recomendados similares al ingresado.
+    
+    Parámetros:
+        - item_id (str): Id del producto para el cual se busca la recomendación. Debe ser un número, ejemplo: 761140
+        
+    Ejemplo de retorno: "['弹炸人2222', 'Uncanny Islands', 'Beach Rules', 'Planetarium 2 - Zen Odyssey', 'The Warrior Of Treasures']"
+
+    """
+    try:
+        item_id = int(item_id) 
+        resultado= recomendacion_por_juego(item_id)
+        return resultado
+    except Exception as e:
+        return {"error":str(e)}
+
+@app.get("/recomendacion_Usuarios/{user_id}", tags=['recomendacion por usuario'])
+async def item(id_usuario: int):
+    """
+    Descripción: Ingresando el id del usuario, devuelve una lista con 5 juegos recomendados para ese usuario
+    
+    Parámetros:
+        - id_usuario (str): Id del producto para el cual se busca la recomendación. Debe ser un número, ejemplo: 761140
+        
+    Ejemplo de retorno: "['弹炸人2222', 'Uncanny Islands', 'Beach Rules', 'Planetarium 2 - Zen Odyssey', 'The Warrior Of Treasures']"
+
+    """
+    try:
+        id_usuario = int(id_usuario) 
+        resultado= recomendacion_por_usuario(id_usuario)
+        return resultado
+    except Exception as e:
+        return {"error":str(e)}
